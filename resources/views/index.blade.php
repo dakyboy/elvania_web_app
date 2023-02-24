05297b16f3213d6e31ff342cd30a1f64289926b4
Dev Dakii <eaaron25@gmail.com>
2023-02-24 23:41:21 +0000

    Formated Document

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,19 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>El'Vania: Bakery. Pastry. Chef</title>
-    <link rel="stylesheet" href="{{secure_asset('/css/app.css')}}"
-</head>
+    <link rel="stylesheet" href="{{ secure_asset('/css/app.css') }}" </head>
+
 <body>
-    <h2>About Me</h2>
-    <x-nav.navbar />
-    <x-content.about_me >
-        <x-slot:title >
-            <h1>Juice</h1>
-        </x-slot>
-        <strong>Whoops!</strong> Something went wrong!
-    </x-content.about_me>
+    <div class="container">
+        <h1 id="title">Elvania</h1>
+        <h2>About Me</h2>
+        <x-nav.navbar />
+        <x-content.about_me>
+            <x-slot:title>
+                <h1>Juice</h1>
+            </x-slot:title>
+            <strong>Whoops!</strong> Something went wrong!
+        </x-content.about_me>
+    </div>
 </body>
+
 </html>
